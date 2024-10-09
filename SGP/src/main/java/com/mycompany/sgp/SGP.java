b7bff784b94376b2479c127c93e52afb3713f3ca
VegaSamuel <samuelvega.122003@gmail.com>
2024-10-08 18:37:28 -0700

    Creación proyecto NetBeans

---
+++ SGP/src/main/java/com/mycompany/sgp/SGP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sgp;
+
+/**
+ *
+ * @author atrap
+ */
+public class SGP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
